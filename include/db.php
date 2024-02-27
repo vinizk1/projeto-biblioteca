faf7e901d2df75e3a3af039cb1d06ebc170b3da0
Vinicius <vinizarkin@gmail.com>
2024-02-27 16:01:55 -0300

    Update db.php

--- include/db.php
+++ include/db.php
@@ -2,7 +2,7 @@
 
 class DB{
     Static function getInstance(){
-        return new PDO("mysql:host=localhost;database=projeto_biblioteca","root","");
+        return new PDO("mysql:host=localhost;dbname=projeto_biblioteca","root","");
     }
 }
 
